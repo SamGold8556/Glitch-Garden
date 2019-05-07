95fe6f684bea8a79bf6055dd0aa9490515264db2
Sam Gold <SamGold8556@gmail.com>
2019-05-06 19:00:02 -0600

    IMported Background Asset Matched Camera to Canvas Size - World Space

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    int currentSceneIndex;
+	// Use this for initialization
+	void Start () {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScreen());
+        }
+    }
+	
+    IEnumerator LoadStartScreen()
+    {
+        yield return new WaitForSeconds(3);
+        LoadNextScene();
+        //SceneManager.LoadScene("Start Screen");
+
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+
+}
